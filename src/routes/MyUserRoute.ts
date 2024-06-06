44d12b0502639cfcdd7e50fbcb64633a4b5f6026
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-06-06 23:16:40 +0530

    Validated Access Token!!

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
+import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
 // api/my/user > on getting this post req this handler will be clled 
-router.post("/", MyUserController.createCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
 
 export default router;
